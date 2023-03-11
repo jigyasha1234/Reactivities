4d35de1127d72a65796852b29a24e584e23e5216
jigyasha rani <jigrani0000@gmail.com>
2023-03-11 17:06:59 +0530

    secsion iv

---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+using Domain;
+
+namespace Application.Core;
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Activity, Activity>();
+    }
+}
